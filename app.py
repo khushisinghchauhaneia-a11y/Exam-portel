e1dfeb59cbc39c23df669a446cd9f9b93381d7ad
Chandan-Shukla1 <emergingindiagroup12@gmail.com>
2025-04-03 12:13:04 +0530

    main

--- app.py
+++ app.py
@@ -24,8 +24,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+
+
+
 # Load configuration from environment variables
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your-secret-key')
+
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'sampleemailidmindsparc@gmail.com')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'tfducgfuhpuxwrsc')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'sampleemailidmindsparc@gmail.com')
+
+# Initialize mail after setting all configs
+mail = Mail(app)
+
+
+
 
 # Update the database URI if needed
 database_url = os.getenv('SQLALCHEMY_DATABASE_URI')
