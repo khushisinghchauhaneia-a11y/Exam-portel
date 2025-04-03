49dd8191ec1f8adc17c5c10226b0c53fe1d610f7
Chandan-Shukla1 <emergingindiagroup12@gmail.com>
2025-04-03 15:29:10 +0530

    main

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'sampleemailidmindsparc@gmail.com')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'tfducgfuhpuxwrsc')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'njoe vdav ltdf cruq')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'sampleemailidmindsparc@gmail.com')
 
 
